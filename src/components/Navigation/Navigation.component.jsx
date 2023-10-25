aae605379b14dd7bd830418f5d36cef6eb03df4b
Grant <gkinghorn3@icloud.com>
2023-10-25 11:33:06 +0100

    navigation route/ component added, styling for desktop and tablet started

--- src/components/Navigation/Navigation.component.jsx
+++ src/components/Navigation/Navigation.component.jsx
@@ -1,11 +1,19 @@
 import './navigation.styles.scss'
 
+import logo from '../../assets/logo.svg';
+
 const Navigation = () => {
     return (
-        <div className='nav-container'>
-            <div>item 1</div>
-            <div> item 1</div>
-        </div>
+        <nav className='nav-container flex'>
+            <div className='heading-left'>
+                <img className='logo-icon' src={logo} alt='Logo'></img>
+            </div>
+            <ul className='nav-links flex underline-indicators' style={{'--gap': '3rem'}}>
+                <li className='numbered-title'><span>01</span>Destinations</li>
+                <li className='numbered-title'><span>02</span>Crew</li>
+                <li className='numbered-title'><span>03</span>Technology</li>
+            </ul>
+        </nav>
         
     )
 }
